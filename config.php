76a3a2eb4e50ab8fc26ca06f79a4711dc67cac9e
BalaRajendran <31990008+BalaRajendran@users.noreply.github.com>
2018-05-05 21:21:49 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$con = mysqli_connect('208.91.198.197:3306', 'BalajiNandha', '@Itengineer007','farmingarms');
+if ($con->connect_error) {
+die("Database Connection failed: " . $con->connect_error);
+}
+?>
